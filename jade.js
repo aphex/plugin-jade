213ae321c3578f21bb09f78d435f9ae8750c6036
Ross Gerbasi <rgerbasi@gmail.com>
2015-07-31 14:24:52 -0500

    Updated for Latest SystemJs without normilzation

--- jade.js
+++ jade.js
@@ -1,17 +1,9 @@
+/* */
+"format cjs";
 var Jade = require('jade-compiler');
-var P_jade_loc = System.normalize('jade-compiler', module.id );
 
-exports.translate = function(load) {
+exports.translate = function (load) {
   var template_fn = Jade.compileClient(load.source);
-  return Promise.resolve(
-    P_jade_loc.then(function(jade_loc) {
-      // Note: mask "require" by separating from  left parenthesis to prevent
-      // dependency processing on module load.
-      //
-      return "var jade = require" + "('" + jade_loc + "/lib/runtime');\n\n" +
-        'module.exports = ' + template_fn;
-    })
-  );
-
+  return "var jade = require" + "('jade-compiler/lib/runtime');\n\n" +
+    'module.exports = ' + template_fn;
 };
-
